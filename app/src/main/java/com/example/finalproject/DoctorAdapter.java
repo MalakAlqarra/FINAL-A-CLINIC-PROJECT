5cad6407db40021513d7e896e590719f71e4027d
Malak Barham <malakalqarra02@gmail.com>
2025-09-28 09:19:11 +0300

    PROJECT CLASESS

--- app/src/main/java/com/example/finalproject/DoctorAdapter.java
+++ app/src/main/java/com/example/finalproject/DoctorAdapter.java
@@ -1,64 +1,64 @@
-package com.example.finalproject;
-
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
-
-public class DoctorAdapter extends RecyclerView.Adapter<DoctorAdapter.DoctorViewHolder> {
-
-    private Context context;
-    private List<Doctor> doctorList;
-    private OnDoctorClickListener listener;
-
-    public interface OnDoctorClickListener {
-        void onBookClick(Doctor doctor);
-    }
-
-    public DoctorAdapter(Context context, List<Doctor> doctorList, OnDoctorClickListener listener) {
-        this.context = context;
-        this.doctorList = doctorList;
-        this.listener = listener;
-    }
-
-    @NonNull
-    @Override
-    public DoctorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.doctor_item, parent, false);
-        return new DoctorViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull DoctorViewHolder holder, int position) {
-        Doctor doctor = doctorList.get(position);
-        holder.tvDoctorName.setText("Dr. " + doctor.getName());
-        holder.tvSpecialty.setText("Specialty: " + doctor.getSpecialty());
-
-        holder.btnBook.setOnClickListener(v -> listener.onBookClick(doctor));
-    }
-
-    @Override
-    public int getItemCount() {
-        return doctorList.size();
-    }
-
-    public static class DoctorViewHolder extends RecyclerView.ViewHolder {
-        TextView tvDoctorName, tvSpecialty;
-        Button btnBook;
-
-        public DoctorViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvDoctorName = itemView.findViewById(R.id.tvDoctorName);
-            tvSpecialty = itemView.findViewById(R.id.tvSpecialty);
-            btnBook = itemView.findViewById(R.id.btnBook);
-        }
-    }
-}
+//package com.example.finalproject;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import java.util.List;
+//
+//public class DoctorAdapter extends RecyclerView.Adapter<DoctorAdapter.DoctorViewHolder> {
+//
+//    private Context context;
+//    private List<Doctor> doctorList;
+//    private OnDoctorClickListener listener;
+//
+//    public interface OnDoctorClickListener {
+//        void onBookClick(Doctor doctor);
+//    }
+//
+//    public DoctorAdapter(Context context, List<Doctor> doctorList, OnDoctorClickListener listener) {
+//        this.context = context;
+//        this.doctorList = doctorList;
+//        this.listener = listener;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public DoctorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(context).inflate(R.layout.doctor_item, parent, false);
+//        return new DoctorViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull DoctorViewHolder holder, int position) {
+//        Doctor doctor = doctorList.get(position);
+//        holder.tvDoctorName.setText("Dr. " + doctor.getName());
+//        holder.tvSpecialty.setText("Specialty: " + doctor.getSpecialty());
+//
+//        holder.btnBook.setOnClickListener(v -> listener.onBookClick(doctor));
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return doctorList.size();
+//    }
+//
+//    public static class DoctorViewHolder extends RecyclerView.ViewHolder {
+//        TextView tvDoctorName, tvSpecialty;
+//        Button btnBook;
+//
+//        public DoctorViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            tvDoctorName = itemView.findViewById(R.id.tvDoctorName);
+//            tvSpecialty = itemView.findViewById(R.id.tvSpecialty);
+//            btnBook = itemView.findViewById(R.id.btnBook);
+//        }
+//    }
+//}
